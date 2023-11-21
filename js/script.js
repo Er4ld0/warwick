bb504ae5171822403607f86fd3b075ba58b01adb
Er4ld0 <junior-eraldo2011@hotmail.com>
2023-11-21 01:10:34 -0300

    git commit -m

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-function clickMenu(){
+function clickRoleJungle(){
     if (rune__1.style.display == 'block'){
         rune__1.style.display = 'none'
     } else{
         rune__1.style.display = 'block'
+    }
+}
+
+function clickRoleTop(){
+    if (rune__2.style.display == 'block'){
+        rune__2.style.display = 'none'
+    } else{
+        rune__2.style.display = 'block'
     }
 }
