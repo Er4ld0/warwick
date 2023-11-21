f17a10973a8c3b2e11112b94647004fc5e1369c1
Er4ld0 <junior-eraldo2011@hotmail.com>
2023-11-21 14:05:34 -0300

    git commit -m

--- js/script.js
+++ js/script.js
@@ -21,3 +21,11 @@ function clickRune(){
         rune.style.display = 'block'
     }
 }
+
+function clickPrecision(){
+    if (precision.style.display == 'block'){
+        precision.style.display = 'none'
+    } else{
+        precision.style.display = 'block'
+    }
+}
